3fe16a28f21e32cb8e4c02a3facd418596fbd30b
SamWalLive <SamWal@live.co.uk>
2016-12-27 12:55:32 +0000

    Initial Import
    Unity files and basic hex grid

---
+++ Assets/Scripts/ToGreen.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+public class ToGreen : MonoBehaviour
+{
+
+    public Material[] materials = new Material[3];
+
+    void Update()
+    {
+
+        if (Input.GetButtonDown("Mouse Primary"))
+        {
+            MouseClick(materials[0]);
+        }
+        else if (Input.GetButtonDown("Mouse Secondery"))
+        {
+            MouseClick(materials[1]);
+        }
+        else if (Input.GetButtonDown("Mouse Wheel"))
+        {
+            MouseClick(materials[2]);
+        }
+
+    }
+
+    void MouseClick(Material mat)
+    {
+
+        int mask = 1 << 8;
+
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit, Mathf.Infinity , mask))
+        {
+            hit.transform.gameObject.GetComponent<Renderer>().material = mat;
+        }
+
+    }
+
+}
