80bd5a8a8f15acfb5e69507a6c6689832f08cfc8
SamWalLive <SamWal@live.co.uk>
2017-02-16 23:29:58 +0000

    Added Tile Assets
    Switched to square grid, added basic level generation

---
+++ Assets/Scripts/TileController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileController : MonoBehaviour
+{
+    public WorldController mainController;
+    public int layer;
+    public float spriteSize;
+    public Vector2 coordinates;
+    public string groundMaterial;
+
+    private int groundMaterialInt;
+    private GameObject childGroundSprite;
+    private float[] rotations = { 0f, 90f, 180f, -90f };
+
+    void SetPosition ()
+    {
+        transform.position = new Vector3(coordinates.x , coordinates.y) * spriteSize;
+    }
+
+    void GenerateGround ()
+    {
+        childGroundSprite = new GameObject("Ground Sprite");
+        childGroundSprite.transform.parent = transform;
+        childGroundSprite.transform.localPosition = Vector3.zero;
+        childGroundSprite.AddComponent<SpriteRenderer>();
+        childGroundSprite.layer = layer;
+        childGroundSprite.transform.rotation = Quaternion.Euler(0f, 0f, rotations[Random.Range(0, rotations.Length)]);
+        groundMaterialInt = Random.Range(0, mainController.groundSprites.GetLength(0));
+        groundMaterial = mainController.spriteNames[groundMaterialInt];
+        childGroundSprite.GetComponent<SpriteRenderer>().sprite = mainController.groundSprites[groundMaterialInt, Random.Range(0, mainController.groundSprites.GetLength(1))];
+    }
+
+	void Start ()
+    {
+        mainController = FindObjectOfType<WorldController>();
+        SetPosition();
+        GenerateGround();
+	}
+	
+}
---
+++ Assets/Scripts/WorldController.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WorldController : MonoBehaviour
+{
+
+    public Vector2 levelSize;
+    public GameObject tile;
+
+    public int arrayLength;
+    public string[] spriteNames;
+    public Sprite[] dirt;
+    public Sprite[] grass;
+    public Sprite[] stone;
+    public Sprite[] sand;
+    public Sprite[] ice;
+    public Sprite[] water;
+
+    public Sprite[,] groundSprites;
+
+    void Start ()
+    {
+        SetArray();
+        GenerateTiles();
+    }
+
+    void SetArray ()
+    {
+        groundSprites = new Sprite[arrayLength, Mathf.Max(water.Length, Mathf.Max(ice.Length, Mathf.Max(sand.Length, Mathf.Max(stone.Length, Mathf.Max(dirt.Length, grass.Length)))))];
+        for (int x = 0; x <= groundSprites.GetLength(1) - 1; x++)
+        {
+            groundSprites[0, x] = dirt[x];
+        }
+        for (int x = 0; x <= groundSprites.GetLength(1) - 1; x++)
+        {
+            groundSprites[1, x] = grass[x];
+        }
+        for (int x = 0; x <= groundSprites.GetLength(1) - 1; x++)
+        {
+            groundSprites[2, x] = stone[x];
+        }
+        for (int x = 0; x <= groundSprites.GetLength(1) - 1; x++)
+        {
+            groundSprites[3, x] = sand[x];
+        }
+        for (int x = 0; x <= groundSprites.GetLength(1) - 1; x++)
+        {
+            groundSprites[4, x] = ice[x];
+        }
+        for (int x = 0; x <= groundSprites.GetLength(1) - 1; x++)
+        {
+            groundSprites[5, x] = water[x];
+        }
+    }
+
+    void GenerateTiles ()
+    {
+        int xLevelSize = Mathf.RoundToInt(levelSize.x);
+        int yLevelSize = Mathf.RoundToInt(levelSize.y);
+
+        for (int i = xLevelSize * -1; i <= xLevelSize; i++)
+        {
+            for (int j = yLevelSize * -1; j <= yLevelSize; j++)
+            {
+                GameObject temp = Instantiate(tile, transform);
+                temp.GetComponent<TileController>().coordinates = new Vector2(i, j);
+                temp.name = "Tile " + new Vector2(i, j).ToString();
+            }
+        }
+
+    }
+
+}
